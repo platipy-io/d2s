fb8d24f54092aaf137342feee8f952093028d6cf
Maxime Vidori <maxime.vidori@gmail.com>
2024-10-05 09:04:48 +0200

    Implement island?

--- main.go
+++ main.go
@@ -7,11 +7,19 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		component := templates.Base(templates.Hello("John"))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		component := templates.Base(templates.Hello("John", nil))
+		component.Render(r.Context(), w)
+	})
+	mux.HandleFunc("/lorem", func(w http.ResponseWriter, r *http.Request) {
+		component := templates.Lorem()
+		if _, ok := r.Header["Hx-Request"]; !ok {
+			component = templates.Base(templates.Hello("John", component))
+		}
 		component.Render(r.Context(), w)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
