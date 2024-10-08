48ef3f303ad3187ade261303825149f8e2e6566c
Maxime Vidori <maxime.vidori@gmail.com>
2024-10-08 22:19:45 +0200

    Log incoming request in server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/IxDay/templ-exp/templates"
@@ -8,18 +9,24 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	logger := func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.Method + " " + r.URL.Path)
+			next(w, r)
+		}
+	}
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", logger(func(w http.ResponseWriter, r *http.Request) {
 		component := templates.Base(templates.Hello("John", nil))
 		component.Render(r.Context(), w)
-	})
-	mux.HandleFunc("/lorem", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	mux.HandleFunc("/lorem", logger(func(w http.ResponseWriter, r *http.Request) {
 		component := templates.Lorem()
 		if _, ok := r.Header["Hx-Request"]; !ok {
 			component = templates.Base(templates.Hello("John", component))
 		}
 		component.Render(r.Context(), w)
-	})
+	}))
 
 	http.ListenAndServe(":8080", mux)
 }
