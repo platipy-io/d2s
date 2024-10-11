de40d0fbbb2d71bf9c3472e3aaa335a51e70a9dd
Maxime Vidori <maxime.vidori@gmail.com>
2024-10-11 09:11:47 +0200

    Otel integration

--- internal/http/middlewares.go
+++ internal/http/middlewares.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/IxDay/templ-exp/internal/logger"
+	"github.com/IxDay/templ-exp/internal/telemetry"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 
 	"github.com/mdobak/go-xerrors"
 )
@@ -19,3 +21,10 @@ func MiddlewareRecover(next http.Handler) http.Handler {
 }
 
 var MiddlewareLogger = logger.Middleware
+
+var MiddlewareOpenTelemetry = otelhttp.NewMiddleware("fooo",
+	otelhttp.WithTracerProvider(telemetry.Provider),
+	otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+		return "server"
+	}),
+)
