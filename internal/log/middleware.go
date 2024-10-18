cb2b4505f9bf87dfe4c7f184e7685865d4eef753
Maxime Vidori <maxime.vidori@gmail.com>
2024-10-18 10:46:44 +0200

    Use chi middleware utils instead of custom one

--- internal/log/middleware.go
+++ internal/log/middleware.go
@@ -4,14 +4,17 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/platipy-io/d2s/internal/http/mutil"
+	"github.com/go-chi/chi/v5/middleware"
 	"go.opentelemetry.io/otel/trace"
 )
 
-func middleware(logger Logger, next http.Handler) http.Handler {
+func handler(logger Logger, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		lw := mutil.WrapWriter(w)
+		ww, ok := w.(middleware.WrapResponseWriter)
+		if !ok {
+			ww = middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		}
 		span := trace.SpanFromContext(r.Context())
 		child := logger.With().
 			Str("span_id", span.SpanContext().SpanID().String()).
@@ -22,22 +25,21 @@ func middleware(logger Logger, next http.Handler) http.Handler {
 			Str("url", r.URL.Path).
 			Str("user_agent", r.UserAgent()).
 			Msg("starting request")
-		defer func() {
-			child.Info().
-				Int("status", lw.Status()).
-				Int("size", lw.BytesWritten()).
-				Dur("elapsed_ms", time.Since(start)).
-				Msg("ending request")
-		}()
+
 		if r.ContentLength != 0 {
 			child.Trace().EmbedObject(Request(r)).Msg("dumping request")
 		}
-		next.ServeHTTP(lw, r.WithContext(child.WithContext(r.Context())))
+		next.ServeHTTP(ww, r.WithContext(child.WithContext(r.Context())))
+		child.Info().
+			Int("status", ww.Status()).
+			Int("size", ww.BytesWritten()).
+			Dur("elapsed_ms", time.Since(start)).
+			Msg("ending request")
 	})
 }
 
 func Middleware(logger Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return middleware(logger, next)
+		return handler(logger, next)
 	}
 }
