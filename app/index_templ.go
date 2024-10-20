1259c0d026b2fe398b574b80045567fde4e15339
Maxime Vidori <maxime.vidori@gmail.com>
2024-10-20 21:16:55 +0200

    Fix editorconfig

--- app/index_templ.go
+++ app/index_templ.go
@@ -36,7 +36,7 @@ func IndexTplt(name string, wrapped templ.Component) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/index.templ`, Line: 4, Col: 74}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/index.templ`, Line: 4, Col: 71}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
